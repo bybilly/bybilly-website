7c53e70aad2345c8241b9cd6cb6f90f731fc48fe
William Lardner <william.lardner0109@gmail.com>
2023-03-02 15:25:15 +0000

    Updated projects

--- src/About.js
+++ src/About.js
@@ -9,8 +9,7 @@ const About = () => {
                 <img className="profile-picture" alt="Billy sitting on a bike and smiling awkwardly" src={billyImg} />
                 <div className="about--text">
                     <h3>Hey!</h3>
-                    <p>I'm Billy. I'm 18, recently left college, and I'm going to university this September.</p>
-                    <p>I have a huge passion for Computer Science and I'd love to have a job working in the field when I graduate.</p>
+                    <p>I'm Billy. I'm a 20 year old student studying Computer Science at the University of Manchester.</p>
                     <p>On this page are a few of my projects, feel free to check them out.</p>
                 </div>
             </div>
--- src/Projects.js
+++ src/Projects.js
@@ -3,7 +3,7 @@ import Project from './Project';
 import uploaderImg from './assets/image-uploader.webp';
 import onepageImg from './assets/onepage-template.webp';
 import portalImg from './assets/portal-template.webp';
-import calculatorImg from './assets/react-calculator.webp';
+import bergenImg from './assets/bergen.webp';
 import steveImg from './assets/stevepert-website.webp';
 import strengImg from './assets/strengmc-website.webp';
 
@@ -14,7 +14,7 @@ const Projects = () => {
             <div className="projects--container">
                 <Project
                     title = "Minecraft Portal Template"
-                    description = "A simple, clean Minecraft 'Portal' template avaliable for free on SpigotMC. Over 19,000 downloads."
+                    description = "A simple, clean Minecraft 'Portal' template avaliable for free on SpigotMC. Over 27,000 downloads."
                     image = {portalImg}
                     imagealt = "Portal main page screenshot"
                     buttons = {[
@@ -24,50 +24,45 @@ const Projects = () => {
                     key = "1"
                 />
                 <Project
-                    title = "Minecraft Onepage Template"
-                    description = "A single page web template avaliable for free on SpigotMC. Over 9000 downloads."
+                    title = "Bergen Drone Footage"
+                    description = "Some drone clips I've put together from my recent trip to Bergen, Norway."
+                    image = {bergenImg}
+                    imagealt = "Image of Bergen"
+                    buttons = {[
+                        {text: "Watch video", link: "https://www.youtube.com/watch?v=0IUwHje_AK0/", id: "1"}
+                    ]}
+                    key = "2"
+                />
+                <Project
+                    title = "Minecraft One Page Template"
+                    description = "A single page web template avaliable for free on SpigotMC. Over 11,000 downloads."
                     image = {onepageImg}
                     imagealt = "Onepage main page screenshot"
                     buttons = {[
                         {text: "Visit on SpigotMC", link: "https://www.spigotmc.org/resources/dusk-%E2%98%85-minecraft-dark-theme-website-%E2%98%85-fully-responsive.51246/", id: "1"}
                     ]}
-                    key = "2"
-                />
-                <Project
-                    title = "Simple Image Uploader"
-                    description = "A simple image uploader which stores images on AWS and is coded using NodeJS"
-                    image = {uploaderImg}
-                    imagealt = "Uploader main page screenshot"
-                    buttons = {[
-                        {text: "Visit", link: "https://uploader.bybilly.uk", id: "1"},
-                        {text: "View Source", link: "https://github.com/bybilly/Image-Uploader", id: "2"}
-                    ]}
                     key = "3"
                 />
                 <Project
-                    title = "React Calculator"
-                    description = "A basic react calculator hosted on Google Firebase using hot-formula-parser to parse the input"
-                    image = {calculatorImg}
-                    imagealt = "React calculator main page screenshot"
+                    title = "Simple Image Uploader"
+                    description = "A simple image uploader which stores images on AWS and is coded using NodeJS."
+                    image = {uploaderImg}
+                    imagealt = "Uploader main page screenshot"
                     buttons = {[
-                        {text: "Visit", link: "https://calculator.bybilly.uk/", id: "1"},
-                        {text: "View Source", link: "https://github.com/bybilly/react-calculator", id: "2"}
+                        {text: "View Source", link: "https://github.com/bybilly/Image-Uploader", id: "2"}
                     ]}
                     key = "4"
                 />
                 <Project
                     title = "StrengMC Website"
-                    description = "The official StrengMC Minecraft server website, integrating with the server and using PHP in the back-end"
+                    description = "The official StrengMC Minecraft server website, which integrated with the Minecraft server and used PHP in the back-end."
                     image = {strengImg}
                     imagealt = "StrengMC home page screenshot"
-                    buttons = {[
-                        {text: "Visit", link: "https://strengmc.com/", id: "1"}
-                    ]}
                     key = "5"
                 />
                 <Project
                     title = "Steve Pert's Website"
-                    description = "The official website of the UK singer Steve Pert"
+                    description = "The official website of the late UK singer Steve Pert. May he rest in peace."
                     image = {steveImg}
                     imagealt = "Steve Pert home page screenshot"
                     buttons = {[
