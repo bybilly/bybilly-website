bb4958448505c46754014efbe4aae7383dde2da1
William Lardner <william.lardner0109@gmail.com>
2024-06-20 23:53:30 +0100

    Reword and ensure age adjusts automatically in about section
    Previously, my age was wrong. There was also no LinkedIn link.
    
    Made age adjust automatically on my birthday.
    
    Added LinkedIn profile.

--- src/About.js
+++ src/About.js
@@ -1,16 +1,18 @@
 import './About.css';
 import billyImg from './assets/billy.webp';
 
-const About = () => {
+const About = ({ age }) => {
     return (
         <div className="about section">
             <h2>About Me</h2>
             <div className="about--section">
                 <img className="profile-picture" alt="Billy sitting on a bike and smiling awkwardly" src={billyImg} />
                 <div className="about--text">
-                    <h3>Hey!</h3>
-                    <p>I'm Billy. I'm a 20 year old student studying Computer Science at the University of Manchester.</p>
+                    <h3>Hello!</h3>
+                    <p>I'm Billy. I'm a { age } year old student studying Computer Science at the University of Manchester.</p>
                     <p>On this page are a few of my projects, feel free to check them out.</p>
+                    <br />
+                    <p>My LinkedIn profile is available at <a href="https://www.linkedin.com/in/william-lardner/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/william-lardner/</a>.</p>
                 </div>
             </div>
         </div>
--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,17 @@ import Projects from './Projects';
 import About from './About';
 import Contact from './Contact';
 
+// Calculate number of years since my birthday
+const birthday = new Date("01 Sep 2002 00:08:00 GMT");
+const birthdayDiff = new Date(Date.now() - birthday);
+const age = birthdayDiff.getUTCFullYear() - 1970;
+
 const Home = () => {
     return (
         <div>
             <Hero />
             <Projects />
-            <About />
+            <About age={age} />
             <Contact />
         </div>
     );
